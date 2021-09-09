906f0da371e7da0d2d25ec2b4e6cfbc47130e113
KalotiTanay <65613954+KalotiTanay@users.noreply.github.com>
2021-09-09 10:16:06 +0530

    Update Student.java

--- Java Assingments/1/Student.java
+++ Java Assingments/1/Student.java
@@ -5,17 +5,16 @@ public class Student {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int marks[][] = new int[3][3];
-        int totalMks=0,sub1=0,sub2=0,sub3=0;
-        System.out.println("");
-
+        int total=0,sub1=0,sub2=0,sub3=0;
+        
         for(int i = 0;i< marks.length;i++)
-        {   totalMks = 0;
-            System.out.println("Entering the details of "+(i+1)+" student. ");
+        {   total = 0;
+            System.out.println("Fill the details of student "+(i));
             for (int j=0;j<marks[i].length;j++)
             {
                 System.out.println("Enter the mark in subject "+(j+1));
                 marks[i][j]= sc.nextInt();
-                totalMks+=marks[i][j];
+                total+=marks[i][j];
                 if (j==0)
                     sub1+=marks[i][j];
                 else if (j==1)
@@ -24,14 +23,10 @@ public class Student {
                     sub3+=marks[i][j];
             }
 
-            System.out.println("\nTOTAL MARKS OBTAINED BY STUDENT "+(i+1)+" is "+totalMks);
-            System.out.println("AVERAGE MARKS OBTAINED BY STUDENT "+(i+1)+" is "+(totalMks/3));
+            System.out.println("\nTOTAL MARKS OBTAINED BY STUDENT "+(i+1)+": "+total);
+            System.out.println("AVERAGE MARKS OBTAINED BY STUDENT "+(i+1)+": "+(total/3));
         }
-        System.out.println("\nTotal Marks in Subject 1   : " +sub1 );
-        System.out.println("Average Marks in Subject 1 : " +(sub1/3) );
-        System.out.println("Total Marks in Subject 2   : " +sub2 );
-        System.out.println("Average Marks in Subject 1 : " +(sub2/3) );
-        System.out.println("Total Marks in Subject 3   : " +sub3 );
-        System.out.println("Average Marks in Subject 3 : " +(sub3/3) );
+        System.out.println("\nTotal Marks:\nSubject 1:\t"+sub1+"\nSubject2:\t"+sub2+"\nSubject3:\t"+sub3 );
+        System.out.println("\nAverage Marks:\nSubject 1:\t"+(sub1/3)+"\nSubject2:\t"+(sub2/3)+"\nSubject3:\t"+(sub3/3));
     }
 }
